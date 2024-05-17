38b90bbf96fbaa8066557de5a771f05c0489de78
ImmaGOOD <42084689+ImmaGOOD@users.noreply.github.com>
2024-05-17 10:03:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ keepAlive();
 function formatTime() { //Credits to himika#0001 and never#0001
   const date = new Date();
   const options = {
-    timeZone: 'America/New_York', //https://www.zeitverschiebung.net/en/ and find your city and enter here
+    timeZone: 'Asia/Bangkok', //https://www.zeitverschiebung.net/en/ and find your city and enter here
     hour12: true,
     hour: 'numeric',
     minute: 'numeric'
@@ -23,19 +23,19 @@ client.on('ready', async () => {
   console.log(`${client.user.tag} - rich presence started!`);
 
   const r = new Discord.RichPresence()
-    .setApplicationId('')
+    .setApplicationId('1240711154741936128')
     .setType('STREAMING')
-    .setURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ') //Must be a youtube video link 
-    .setState('Recording')
-    .setName('')
-    .setDetails(`Valorant [${formatTime()}]`)
+    .setURL('https://www.youtube.com/watch?v=QRF9TgkBCjc') //Must be a youtube video link 
+    .setState('Spanking')
+    .setName('DeliriumHD')
+    .setDetails(`One night Stand `)
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('') //You can put links in tenor or discord and etc.
-    .setAssetsLargeText('Boring') //Text when you hover the Large image
-    .setAssetsSmallImage('') //You can put links in tenor or discord and etc.
-    .setAssetsSmallText('Twitch') //Text when you hover the Small image
-    .addButton('Watch', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-    .addButton('Donate', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+    .setAssetsLargeImage('https://media.discordapp.net/attachments/1227880335069548604/1240713219169849455/ezgif-6-87e23df9ab.gif?ex=66478fba&is=66463e3a&hm=8498880f167adf7574771b28975167f47fbc2074f5a60e37a0de22c173c51dfb&=') //You can put links in tenor or discord and etc.
+    .setAssetsLargeText('BDSM') //Text when you hover the Large image
+    .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1227880335069548604/1240719319529295872/193c648ab15187e2e23e968f5f2bdc44.gif?ex=66479569&is=664643e9&hm=82c317ab04322cf86cf23528eed864afd1e812dbdc4d3ef3544ab158da150269&') //You can put links in tenor or discord and etc.
+    .setAssetsSmallText('DOM') //Text when you hover the Small image
+    .addButton('Instagram', 'https://www.instagram.com/deliriumhd/')
+
 
   client.user.setActivity(r);
   client.user.setPresence({ status: "dnd" }); //dnd, online, idle, offline
@@ -44,12 +44,12 @@ client.on('ready', async () => {
   setInterval(() => {
     const newTime = formatTime();
     if (newTime !== prevTime) {
-      const newDetails = ` [${newTime}]`;
+      const newDetails = `DM 24/7`;
       r.setDetails(newDetails);
       client.user.setActivity(r);
       prevTime = newTime;
     }
-  }, 1000); // Update every second
+  }, 500); // Update every second
 });
 
 const mySecret = process.env['TOKEN'];
